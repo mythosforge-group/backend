02cebd0099745413881b23fdaee58629cd39450d
ianco <ianco.soares.labcomu@imd.ufrn.br>
2025-04-18 22:03:00 -0300

    :ok_hand: style: ajustes na identação

--- src/main/java/mythosforge/fable_minds/repository/UserRepository.java
+++ src/main/java/mythosforge/fable_minds/repository/UserRepository.java
@@ -1,15 +1,12 @@
 package mythosforge.fable_minds.repository;
 
-import mythosforge.fable_minds.models.User;
+import mythosforge.fable_minds.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-
-    Optional<User> findByUsername(String username);
-
-
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+    
+    Optional<Users> findByEmail(String email);
 }
 
